e3cb1b95e7aec6b56a4c00bf608a91e45367b48d
Khun <favefan@live.com>
2024-04-19 14:11:50 +0800

    Formatted and fixed incorrect comparison between pointer and int in AsyncLogging

--- src/base/Logging.h
+++ src/base/Logging.h
@@ -4,49 +4,46 @@
 
 class Logger;
 
-#define LOG_ERROR(format, ...) \
-    if(Logger::LogError <= Logger::getLogLevel()) \
-        Logger().write(Logger::LogError, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+// __FILE__，__FUNCTION__ 和 __LINE__
+// 是C++的预处理器宏，它们分别表示当前源文件的名称，当前函数的名称和当前代码行号。
+#define LOG_ERROR(format, ...)                                                 \
+  if (Logger::LogError <= Logger::getLogLevel())                               \
+  Logger().write(Logger::LogError, __FILE__, __FUNCTION__, __LINE__, format,   \
+                 ##__VA_ARGS__)
 
-#define LOG_WARNING(format, ...) \
-    if(Logger::LogWarning <= Logger::getLogLevel()) \
-        Logger().write(Logger::LogWarning, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define LOG_WARNING(format, ...)                                               \
+  if (Logger::LogWarning <= Logger::getLogLevel())                             \
+  Logger().write(Logger::LogWarning, __FILE__, __FUNCTION__, __LINE__, format, \
+                 ##__VA_ARGS__)
 
-#define LOG_DEBUG(format, ...) \
-    if(Logger::LogDebug <= Logger::getLogLevel()) \
-        Logger().write(Logger::LogDebug, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define LOG_DEBUG(format, ...)                                                 \
+  if (Logger::LogDebug <= Logger::getLogLevel())                               \
+  Logger().write(Logger::LogDebug, __FILE__, __FUNCTION__, __LINE__, format,   \
+                 ##__VA_ARGS__)
 
-
-
-class Logger
-{
+class Logger {
 public:
-    enum LogLevel
-    {
-        LogError,
-        LogWarning,
-        LogDebug
-    };
+  enum LogLevel { LogError, LogWarning, LogDebug };
 
-    Logger();
-    ~Logger();
+  Logger();
+  ~Logger();
 
-    static void setLogFile(std::string file);
-    static std::string getLogFile();
-    static void setLogLevel(LogLevel level);
-    static LogLevel getLogLevel();
+  static void setLogFile(std::string file);
+  static std::string getLogFile();
+  static void setLogLevel(LogLevel level);
+  static LogLevel getLogLevel();
 
-    void write(LogLevel level, const char* file, const char* func,
-                                int line, const char* format, ...);
+  void write(LogLevel level, const char *file, const char *func, int line,
+             const char *format, ...);
 
 private:
-    char mData[4096];
-    char* mCurPtr;
-    LogLevel mThisLogLevel;
+  char mData[4096];
+  char *mCurPtr;
+  LogLevel mThisLogLevel;
 
-    static LogLevel mLogLevel;
-    static std::string mLogFile;
-    static bool mIsStdout;
+  static LogLevel mLogLevel;
+  static std::string mLogFile;
+  static bool mIsStdout;
 };
 
 #endif //_LOGING_H_
